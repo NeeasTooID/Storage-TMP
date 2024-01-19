ff960be882e0aa6818379516bc120d2ff8d90bd2
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-01-19 23:11:46 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,9 +8,19 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (item.type === "file") {
                     const listItem = document.createElement("li");
                     const link = document.createElement("a");
+                    const rawLink = document.createElement("a");
+
                     link.href = item.html_url;
                     link.textContent = item.name;
+
+                    rawLink.href = item.download_url;
+                    rawLink.textContent = "View Raw";
+                    rawLink.setAttribute("target", "_blank");
+
                     listItem.appendChild(link);
+                    listItem.appendChild(document.createTextNode(" | "));
+                    listItem.appendChild(rawLink);
+
                     fileList.appendChild(listItem);
                 }
             });
