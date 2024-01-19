e54c31bd0f84a1a86fdf0d66a6cd3fd9912af632
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-01-19 22:37:28 +0700

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Menggunakan middleware express.static untuk menyajikan file statis di folder 'public'
-app.use(express.static(path.join(__dirname, 'public')));
