396e149ea0a019b3e39fa26203139268303fa32f
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-01-19 22:33:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,3 @@ const port = process.env.PORT || 3000;
 
 // Menggunakan middleware express.static untuk menyajikan file statis di folder 'public'
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
