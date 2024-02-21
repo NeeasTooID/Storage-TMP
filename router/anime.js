ee85f40ccc9bfdf4aa15bc868c0f59935df7365d
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-22 04:05:50 +0700

    Update anime.js

--- router/anime.js
+++ router/anime.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    // Logika untuk menangani permintaan untuk games
-    res.send('Ini adalah halaman tester anime');
-});
+// Middleware untuk menyajikan file statis dari folder 'public/anime'
+router.use(express.static(path.join(__dirname, '..', 'public', 'anime')));
 
 module.exports = router;
