dd92ca3aca6edb49e6867879d7d759da41259b86
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-22 05:42:49 +0700

    Update and rename rmaid.js to rloli.js

--- router/rloli.js
+++ router/rloli.js
@@ -5,7 +5,7 @@ const request = require('request');
 const path = require('path');
 
 router.get('/', (req, res) => {
-    const dataFilePath = path.join(__dirname, '..', 'apijson', 'anime', 'maid.json');
+    const dataFilePath = path.join(__dirname, '..', 'apijson', 'anime', 'loli.json');
     try {
         const rawData = fs.readFileSync(dataFilePath);
         const data = JSON.parse(rawData);
