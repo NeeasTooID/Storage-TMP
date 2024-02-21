53bf4abca31000e5e959d5b62ae90ebff9cb52fd
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-22 05:42:23 +0700

    Delete router/anime.js

--- router/anime.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const router = express.Router();
-
-// Middleware untuk menyajikan file statis dari folder 'public/anime'
-router.use(express.static(path.join(__dirname, '..', 'public', 'anime')));
-
-module.exports = router;
