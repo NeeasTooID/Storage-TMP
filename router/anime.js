ed98d92e74f0d9bedafa61b66793c1596aa6d71e
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-22 03:52:05 +0700

    Update anime.js

--- router/anime.js
+++ router/anime.js
@@ -1,41 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
+const request = require('request');
 
-// Simpan URL gambar yang dipilih sebelumnya di dalam cache
-let previousImageUrl = null;
-
-router.get('/', async (req, res) => {
-    // Baca data dari file JSON
-    const dataFilePath = path.join(__dirname, '..', 'apijson', 'anime', 'waifu.json');
+router.get("/anime", async (req, res, next) => {
+    const dataFilePath = path.join(__dirname, '..', '..', 'apijson', 'anime', 'waifu.json');
     try {
         const rawData = fs.readFileSync(dataFilePath);
         const data = JSON.parse(rawData);
 
-        // Ambil semua URL gambar dari data JSON
-        const imageUrls = data;
-
         // Ambil URL gambar secara acak dari data JSON
-        let imageUrl = previousImageUrl;
-        while (imageUrl === previousImageUrl) {
-            const randomIndex = Math.floor(Math.random() * imageUrls.length);
-            imageUrl = imageUrls[randomIndex];
-        }
-        previousImageUrl = imageUrl;
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const imageUrl = data[randomIndex];
 
         // Lakukan HTTP request untuk mengunduh gambar dari URL
-        const response = await axios.get(imageUrl, {
-            responseType: 'arraybuffer'
+        const requestSettings = {
+            url: imageUrl,
+            method: "GET",
+            encoding: null
+        };
+        request(requestSettings, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.set("Content-Type", "image/png");
+                res.send(body);
+            } else {
+                res.status(500).json({ error: 'Failed to fetch image' });
+            }
         });
-
-        // Kirim gambar sebagai respons dengan tipe konten yang sesuai
-        res.set('Content-Type', response.headers['content-type']);
-        res.send(response.data);
     } catch (error) {
-        console.error('Failed to read data from JSON file or download image:', error);
-        res.status(500).send('Failed to load image');
+        console.error('Failed to read data from JSON file:', error);
+        res.status(500).json({ error: 'Failed to load image' });
     }
 });
 
