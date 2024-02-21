4fcbe32a8d3654d8e0687d2295becfadc6bd9b41
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-22 03:56:04 +0700

    Update anime.js

--- router/anime.js
+++ router/anime.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 const fs = require('fs');
 const request = require('request');
+const path = require('path');
 
-router.get("/anime", async (req, res, next) => {
-    const dataFilePath = path.join(__dirname, '..', '..', 'apijson', 'anime', 'waifu.json');
+router.get('/', (req, res) => {
+    const dataFilePath = path.join(__dirname, '..', 'apijson', 'anime', 'waifu.json');
     try {
         const rawData = fs.readFileSync(dataFilePath);
         const data = JSON.parse(rawData);
@@ -17,12 +17,12 @@ router.get("/anime", async (req, res, next) => {
         // Lakukan HTTP request untuk mengunduh gambar dari URL
         const requestSettings = {
             url: imageUrl,
-            method: "GET",
+            method: 'GET',
             encoding: null
         };
         request(requestSettings, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                res.set("Content-Type", "image/png");
+                res.set('Content-Type', 'image/png');
                 res.send(body);
             } else {
                 res.status(500).json({ error: 'Failed to fetch image' });
