992d0df2771e16e07bd1ae555f4b508a9dbe9d80
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-22 03:17:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 8080; // Ganti dengan port yang Anda inginkan
+const port = 8080;
 
-// Menggunakan middleware untuk menyajikan file statis dari folder 'public'
+const animeRouter = require('./router/anime');
+const gamesRouter = require('./router/games');
+const pinterestRouter = require('./router/pinterest');
+
+// Middleware untuk menyajikan file statis dari folder 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Gunakan router untuk masing-masing kategori
+app.use('/anime', animeRouter);
+app.use('/games', gamesRouter);
+app.use('/pinterest', pinterestRouter);
 
 // Mengalihkan semua permintaan yang tidak cocok dengan file statis ke halaman beranda (index.html)
 app.get('*', (req, res) => {
