e47eea5be7b2d5be645bb4e359ef94ce1bef2b18
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-22 05:24:03 +0700

    Update and rename games.js to nrandom.js

--- router/games.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    // Logika untuk menangani permintaan untuk games
-    res.send('Ini adalah halaman games');
-});
-
-module.exports = router;
