772ca111ebb06fc811d76bc68fd55979c439a1a3
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-05-17 23:11:55 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,43 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+module.exports = {
+  publicRuntimeConfig: {
+    apiEndpoints: [
+      {
+        method: 'GET',
+        path: '/api/items',
+        description: 'Fetches a list of items',
+        curl: `curl -X GET https://api.example.com/api/items`,
+        response: `[
+  {
+    "id": 1,
+    "name": "Item 1",
+    "description": "Description of item 1"
+  },
+  {
+    "id": 2,
+    "name": "Item 2",
+    "description": "Description of item 2"
+  }
+]`
+      },
+      {
+        method: 'POST',
+        path: '/api/items',
+        description: 'Creates a new item',
+        curl: `curl -X POST https://api.example.com/api/items -H "Content-Type: application/json" -d '{
+  "name": "New Item",
+  "description": "Description of new item"
+}'`,
+        requestBody: `{
+  "name": "New Item",
+  "description": "Description of new item"
+}`,
+        response: `{
+  "id": 3,
+  "name": "New Item",
+  "description": "Description of new item"
+}`
+      }
+    ]
+  }
+}
