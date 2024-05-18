2f93b8a1ae2216f73900106dcd4aeba56673d163
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-05-19 03:26:19 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,7 @@
 module.exports = {
-  publicRuntimeConfig: {
-    apiEndpoints: [
-      {
-        method: 'GET',
-        path: '/api/items',
-        description: 'Fetches a list of items',
-        curl: `curl -X GET https://api.example.com/api/items`,
-        response: `[
-  {
-    "id": 1,
-    "name": "Item 1",
-    "description": "Description of item 1"
-  },
-  {
-    "id": 2,
-    "name": "Item 2",
-    "description": "Description of item 2"
+  greeting: "Hello, world!",
+  numbers: [1, 2, 3, 4, 5],
+  sayHello: function() {
+    console.log("Hello!");
   }
-]`
-      },
-      {
-        method: 'POST',
-        path: '/api/items',
-        description: 'Creates a new item',
-        curl: `curl -X POST https://api.example.com/api/items -H "Content-Type: application/json" -d '{
-  "name": "New Item",
-  "description": "Description of new item"
-}'`,
-        response: `{
-  "id": 3,
-  "name": "New Item",
-  "description": "Description of new item"
-}`
-      }
-    ]
-  }
-}
+};
